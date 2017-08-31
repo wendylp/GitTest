22971205cde1e034b21f1a32e0de3fc159791c71
wendylp <wendylp@yahoo.com>
2017-08-31 13:40:13 +0800

    test git02

--- src/com/atguigu/git/HelloGit.java
+++ src/com/atguigu/git/HelloGit.java
@@ -1,10 +1,13 @@
 package com.atguigu.git;
 
+
 public class HelloGit {
 
 	public static void main(String[] args) {
 		
 		System.out.println("hello Git...111111111111111111111");
+		
+		System.out.println("test git02");
 	}
 
 }
